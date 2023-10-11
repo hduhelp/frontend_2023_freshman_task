8129a366b90e240af714c766978856a8e1c2a6b8
float <gxhurry@outlook.com>
2023-10-11 22:59:51 +0800

    router

--- fltb/src/main.js
+++ fltb/src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import Particles from "vue3-particles";
 import App from './App.vue'
-
+import router from './router';
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import 'animate.css';
 
-createApp(App).use(Particles).mount('#app')
+createApp(App)
+    .use(router)
+    .use(Particles)
+    .mount('#app');
